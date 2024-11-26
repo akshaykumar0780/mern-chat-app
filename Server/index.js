de182417ee103977c1f354e5355ecc49263e0f8c
Akshay Kumar <akshay.kumar0780238@gmail.com>
2024-11-26 17:25:56 +0530

    changing env variable

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 //middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://mern-chat-app-three-xi.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
