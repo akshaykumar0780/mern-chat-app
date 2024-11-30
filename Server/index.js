267d117b041f7293c37903f5c27fb1c2bd69e33e
Akshay Kumar <akshay.kumar0780238@gmail.com>
2024-11-30 23:51:12 +0530

    updating index.js

--- Server/index.js
+++ Server/index.js
@@ -16,21 +16,29 @@ const app = express();
 
 //middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://localhost:5173");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  }
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Respond OK to preflight requests
+  }
   next();
 });
 
-app.options(
-  "*",
-  cors({
-    origin: "https://localhost:5173",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
+app.options("*", (req, res) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  }
+  res.sendStatus(200);
+});
 
 app.use(cookieParser());
 app.use(express.json());
