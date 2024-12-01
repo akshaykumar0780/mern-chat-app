cf986e18c4927dda2f006633a9e16d90725c759d
Akshay Kumar <akshaykumar0780238@gmail.com>
2024-12-01 12:10:55 +0530

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
-    
   },
-})
+  server: {
+    host: '0.0.0.0',  // Allow external connections (important for Render)
+    port: process.env.PORT || 3000,  // Use the port specified by Render or default to 3000
+  },
+});
